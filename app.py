857d7028fcff9fb27968e987b360fe886b7c13e6
Eric Ma <ericmajinglong@gmail.com>
2018-03-11 18:13:17 -0400

    Removed unnecessary lines.

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from jinja2 import Template
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def view_resume():
     with open('resume.yaml', 'r+') as f:
@@ -23,7 +22,7 @@ def view_resume():
 
     with open('index.html', 'w+') as f:
         f.write(t.render(resume=resume))
-        
+
     return render_template('resume.html.j2', resume=resume)
 
 
