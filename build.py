f615bdd3364e54bef5a72c1eb8a36d6a351a4d69
Eric Ma <ericmajinglong@gmail.com>
2021-05-02 19:16:29 -0400

    Add bayesian decision-making workflow preprint to resume

--- build.py
+++ build.py
@@ -4,13 +4,13 @@ Build script for resume HTML version.
 import os
 
 from app import write_html, read_resume
+import shutil
 
+shutil.rmtree("site/")
 os.makedirs("site/", exist_ok=True)
 
 resume = read_resume()
 write_html(resume, path="site/index.html")
 
-import shutil
 
-
-shutil.copytree("static", "site/static", )
+shutil.copytree("static", "site/static")
